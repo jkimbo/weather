04c454ce08fec9de57089c5b2b6cdb614ef5dfd7
Jonathan Kim <jkimbo@gmail.com>
2011-11-17 16:09:54 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Author: J.Kim
+
+*/
+
+$(document).ready(function() {
+    var FullscreenrOptions = {  width: 1200, height: 802, bgID: '#bgimg' };
+    $.fn.fullscreenr(FullscreenrOptions);
+    $('body').height($(window).height());
+});
+
+
+
+
